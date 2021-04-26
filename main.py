69880d3f2466bb3167d16449919eb1e9fb8f176f
RebootLinux <trollenslager@gmail.com>
2021-04-26 12:28:48 +0100

    added input

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def for_function():
-	for x in range(1, 10):
-    		print("changed by Rebootlinux")
 
-for_function()
+def for_function(x):
+	for y in range(1, x+1):
+            print(y)
+
+def getInput():
+    _max = int(input("Give the amount to loop: "))
+    for_function(_max)
+
+
+getInput()
